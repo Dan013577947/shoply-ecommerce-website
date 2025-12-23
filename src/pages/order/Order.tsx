4ed30014404a07f418054bf6d4658a17a89ff941
Dan013577947 <93627888+Dan013577947@users.noreply.github.com>
2025-12-23 11:44:54 +0800

    included cancel order button in Order page

--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -47,32 +47,38 @@ export default function Order({ orders, carts, setCarts, handleSearchResult, han
                   {order.orders.map(order => {
                     return (
                       <div key={order.id} className="p-2">
-                        <div className="flex items-center">
+                        <div className="flex">
                           <div className="w-[25%]">
-                            <div>
+                            <div className="flex items-center justify-center h-full">
                               <img src={order.image} alt={order.title} className="w-35" />
                             </div>
                           </div>
                           <div className="w-[45%] py-3">
-                            <div className="h-full flex flex-col justify-between">
-                              <div className="font-[500] text-[17px]">
-                                {order.title}
-                              </div>
-                              <div className="flex">
-                                <div  className="mr-1">Arriving on:</div>
-                                <div>{order.deliveryDate}</div>
-                              </div>
-                              <div className="flex">
-                                <div className="mr-1">Quantity:</div>
-                                <div>{order.quantity}</div>
-                              </div>
-                              <div>
-                                <button className="bg-yellow-300 cursor-pointer px-5 py-2 rounded-[8px] shadow-[0_0_4px_rgba(0,0,0,0.1)]">Buy it again</button>
+                            <div className="flex items-center h-full">
+                              <div className="h-full flex flex-col justify-between">
+                                <div className="font-[500] text-[17px]">
+                                  {order.title}
+                                </div>
+                                <div className="flex">
+                                  <div className="mr-1">Arriving on:</div>
+                                  <div>{order.deliveryDate}</div>
+                                </div>
+                                <div className="flex">
+                                  <div className="mr-1">Quantity:</div>
+                                  <div>{order.quantity}</div>
+                                </div>
+                                <div>
+                                  <button className="bg-yellow-300 cursor-pointer px-5 py-2 rounded-[8px] shadow-[0_0_4px_rgba(0,0,0,0.1)]">Buy it again</button>
+                                </div>
                               </div>
                             </div>
                           </div>
                           <div className="w-[30%]">
-                            <button className="border border-gray-300 px-20 py-2 rounded-[6px] shadow-[0_0_4px_rgba(0,0,0,0.2)] cursor-pointer">Tracking Page</button>
+                            <div className="flex flex-col items-center justify-evenly h-full">
+                              <button className="border border-gray-300 px-24 py-2 rounded-[6px] shadow-[0_0_2px_rgba(0,0,0,0.2)] cursor-pointer">Track Page</button>
+                              <button className="px-10 py-2 rounded-[6px] bg-red-600 text-white shadow-[0_0_2px_rgba(0,0,0,0.2)] cursor-pointer">Cancel Order</button>
+                            </div>
+
                           </div>
                         </div>
                       </div>
